8f98b56d4068b47932c4987309f6a3393d3714c7
ainayeem-pc <ain.nayeem1999@gmail.com>
2024-12-28 13:12:34 +0600

    items saved into  cookie

--- app/Zustand/useItemStore.jsx
+++ app/Zustand/useItemStore.jsx
@@ -1,12 +1,30 @@
+import Cookies from "js-cookie";
 import { create } from "zustand";
 
+const COOKIE_KEY = "selectedOptions";
+
 const useItemStore = create((set) => ({
-  selectedOptions: [],
+  selectedOptions: (() => {
+    try {
+      const cookieData = Cookies.get(COOKIE_KEY);
+      return cookieData ? JSON.parse(cookieData) : [];
+    } catch (err) {
+      // console.error("Error cookie:", err);
+      return [];
+    }
+  })(),
+
   addSelectedOptions: (item) =>
-    set((state) => ({
-      selectedOptions: [...state.selectedOptions, item],
-    })),
-  clearSelectedOptions: () => set(() => ({ selectedOptions: [] })),
+    set((state) => {
+      const updatedOptions = [...state.selectedOptions, item];
+      Cookies.set(COOKIE_KEY, JSON.stringify(updatedOptions));
+      return { selectedOptions: updatedOptions };
+    }),
+
+  clearSelectedOptions: () => {
+    Cookies.remove(COOKIE_KEY);
+    set(() => ({ selectedOptions: [] }));
+  },
 }));
 
 export default useItemStore;
